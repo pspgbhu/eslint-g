ceb7a9ec3c311509325358b6379c10d7abbed817
hucong <hucong@jd.com>
2017-07-28 11:16:05 +0800

    Update react rules

--- rc/reactrc.js
+++ rc/reactrc.js
@@ -9,11 +9,9 @@ const plugins = [
 rc =
 `
 module.exports = {
-  root: true,
   "parser": "babel-eslint",
   "plugins": [
-    "react",
-    "import"
+    "react"
   ],
   "parserOptions": {
     "ecmaVersion": 6,
@@ -24,29 +22,23 @@ module.exports = {
   },
   "env": {
     "browser": true,
+    "amd": true,
+    "es6": true,
     "node": true,
+    "mocha": true
   },
-  "settings": {
-    "import/resolver": "webpack"
-  },
-  extends: 'airbnb-base',
-  // add your custom rules here
-  rules: {
-    "no-restricted-syntax": ["off", "BinaryExpression[operator='in']"],
-    "import/imports-first": 0,
-    'import/no-dynamic-require': 0,
-    "no-unused-vars": 1,
-    "no-console": 0,
+  "rules": {
+    "comma-dangle": 1,
+    "quotes": [ 1, "single" ],
+    "no-undef": 1,
+    "global-strict": 0,
+    "no-extra-semi": 1,
     "no-underscore-dangle": 0,
-    "arrow-parens": 0,
-    "no-param-reassign": 0,
-    "padded-blocks": 0,
-    "prefer-const": 1,
-    "linebreak-style":0,
-    "max-len":0,
-    "class-methods-use-this": 0,
-    "no-shadow": ["error", { "allow": ["err", "error"] }],
-    "func-names": 0,
+    "no-console": 0,
+    "no-unused-vars": 1,
+    "no-trailing-spaces": [1, { "skipBlankLines": true }],
+    "no-unreachable": 1,
+    "no-alert": 0,
     "react/jsx-uses-react": 1,
     "react/jsx-uses-vars": 1
   }
