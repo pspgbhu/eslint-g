13b608349a8b9f1a3042d39f7ecfe3b29f8b2e2b
hucong <hucong@jd.com>
2018-03-08 14:35:27 +0800

    feat: update common rules

--- rc/commonrc.js
+++ rc/commonrc.js
@@ -1,5 +1,7 @@
 module.exports =
 `
+    "global-require": 0,
     "prefer-template": 0,
     "no-unused-vars": 1,
+    "no-underscore-dangle": 0,
     "no-use-before-define": ["error", { "functions": false }], `;
